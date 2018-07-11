2b4e4562d03288b60b5daa0e12e3bf6dadf20af6
reginensenda <41118133+reginensenda@users.noreply.github.com>
2018-07-11 19:42:20 +0200

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+
+/**
+ * @author a224404
+ *
+ */
+
+public class Employee {
+	private String employeeName;
+	private int employeeID;
+	
+
+	/*public String getEmployeeName() {
+		return employeeName;
+	}
+
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+
+	public int getEmployeeID() {
+		return employeeID;
+	}
+
+
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+*/
+
+	/**
+	 * 
+	 */
+	public Employee( String empName, int empId) {
+		this.employeeID = empId; 
+		this.employeeName = empName;
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	
+
+	}
+
+
